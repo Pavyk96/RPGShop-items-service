e656f3c0bb35f58b906ea70c60d8d646753eefb9
daniil <pepethefrog27@mail.ru>
2025-03-31 19:24:43 +0500

    Init

---
+++ src/main/java/com/payk96/rpg_shop/RpgShopApplication.java
@@ -0,0 +1,13 @@
+package com.payk96.rpg_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payk96/rpg_shop/RpgShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payk96.rpg_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
