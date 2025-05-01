3769b721fc3fb2701f6e570c5f9a262c623cb5dc
daniil <pepethefrog27@mail.ru>
2025-05-01 11:08:14 +0500

    Защитил эндпоинты

--- src/main/java/payk96/rpg_shop/RpgShopApplication.java
+++ src/main/java/payk96/rpg_shop/RpgShopApplication.java
@@ -3,8 +3,10 @@ package payk96.rpg_shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class RpgShopApplication {
 
 	public static void main(String[] args) {
